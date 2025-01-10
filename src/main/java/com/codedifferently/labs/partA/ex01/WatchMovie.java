5a3cd861277fa1f61778d3dd43f6ad460ee0ecc8
Clamjosh <Clamjosh1@users.noreply.github.com>
2025-01-10 13:03:01 -0500

    finished

--- src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/WatchMovie.java
@@ -1,19 +1,29 @@
 package com.codedifferently.labs.partA.ex01;
 
+import java.util.Scanner;
+
 public class WatchMovie {
     public static String watchMovie() {
-        String response ="";
-        /* Your code goes here*/
+        String response = "";
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the movie ticket price: ");
+        double price = scanner.nextDouble();
 
-        response += ("Enter the movie ticket price \n");
+        System.out.println("Enter the movie rating: ");
+        int rating = scanner.nextInt();
+
+        if (price >= 12 && rating == 5) {
+            response = "I’m interested in watching the movie";
+        } else {
+            response = "I am not interested in watching the movie";
+        }
+
         return response;
-
     }
-    public static void main(String args[]) {
+
+    public static void main(String[] args) {
         String movieOutput = watchMovie();
         System.out.println(movieOutput);
-
-
     }
 }
